aa25f8ac2673f8fcace5aa9a99d430522a02cc7a
Purv Gohel <purv.gohel@tcs.com>
2022-05-10 17:45:32 +0530

    Add project files.

---
+++ Domain/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopbridge_base.Domain.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Product_Id { get; set; }
+    }
+}
